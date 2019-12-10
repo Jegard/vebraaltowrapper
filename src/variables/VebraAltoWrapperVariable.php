9a02a2845c6e16d072a69652f9b70fd8676bf7ca
michaelbutler1998 <michaelspay.pal@hotmail.co.uk>
2019-12-10 13:30:18 +0000

    Adding Floor Plan - Variable

--- src/variables/VebraAltoWrapperVariable.php
+++ src/variables/VebraAltoWrapperVariable.php
@@ -96,7 +96,8 @@ class VebraAltoWrapperVariable
             'description' => 'description',
 
             'bullets,bullet' => 'bullets,bullet',
-            'brochure' => 'brochure'
+            'brochure' => 'brochure',
+            'floorplan'=>'floorplan'
 
         );
     }
